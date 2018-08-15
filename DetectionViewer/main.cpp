3da26dd4360032a6755560e362657985d2d69e77
Lucas Wals Ochoa <32473858+lukaswals@users.noreply.github.com>
2018-08-15 17:28:38 +0900

    Add project files.

---
+++ DetectionViewer/main.cpp
@@ -0,0 +1,17 @@
+/*
+* A simple viewer for Detections provided by different tracking datasets.
+* Written in C++ and making use of openCV
+*
+* Author: Lucas Wals
+*/
+#pragma once
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <iomanip>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello there" << std::endl;
+}
